10e3c300aebcb433a94cff2159f8439575cfb4aa
Frederico Reis <reis@devgrid.co.uk>
2021-03-12 18:40:50 -0300

    chore: enviroment setuped

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './styles/global.scss'
+
+export function App () {
+  // throw new Error('Deu zica');
+
+  return <h1>Until next year(2022), I'll be speaking English fluently and confidently, and also will get a salary above U$$7k, monthly. In Jesus' name!</h1>
+}
